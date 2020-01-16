51dba6b6024dafe586cfa36bee3b5a2ab8fb31e6
Cai Jenkins <Caisjenkins@gmail.com>
2020-01-16 18:38:43 +0000

    added a fewprojects

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,17 +7,21 @@ module.exports = {
   ],
   // Customize your site metadata:
   siteMetadata: {
-    title: `My Blog Title`,
-    author: `My Name`,
-    description: `My site description...`,
+    title: `My Portfolio`,
+    author: `Cai Jenkins`,
+    description: `My previously completed projects`,
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/gatsbyjs`,
+        url: `https://twitter.com/Barbarjinks1`,
       },
       {
         name: `github`,
-        url: `https://github.com/gatsbyjs`,
+        url: `https://github.com/Barbarjinks`,
+      },
+      {
+        name: `linkedin`,
+        url: `https://www.linkedin.com/in/caijenkins/`,
       },
     ],
   },
